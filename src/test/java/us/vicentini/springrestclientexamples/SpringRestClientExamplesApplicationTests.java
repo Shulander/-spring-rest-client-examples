d673712534a7594d31b0f0d577b842a49091bbaa
Henrique Vicentini <henrique@vicentini.us>
2020-01-12 13:00:31 -0800

    class 23.372: Spring Rest Client Examples

---
+++ src/test/java/us/vicentini/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -0,0 +1,19 @@
+package us.vicentini.springrestclientexamples;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestClientExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		log.info("testing spring context load");
+	}
+
+}
