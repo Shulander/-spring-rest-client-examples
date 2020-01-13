591f79609ded708e77ff0ce98a0f21f5ffcf7712
Henrique Vicentini <henrique@vicentini.us>
2020-01-12 19:24:03 -0800

    class 23.376: URI Components Builder

--- src/main/java/us/vicentini/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/us/vicentini/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,12 +1,15 @@
 package us.vicentini.springrestclientexamples.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import us.vicentini.springrestclientexamples.api.domain.User;
 import us.vicentini.springrestclientexamples.api.domain.UserData;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -15,13 +18,20 @@ import java.util.List;
 @ConditionalOnBean(RestTemplate.class)
 class ApiServiceImpl implements ApiService {
 
+    @Value("${api.url}")
+    private String apiUrl;
+
     private final RestTemplate restTemplate;
 
 
     @Override
     public List<User> getUsers(Integer limit) {
-        String url = "http://private-anon-1eb7500bda-apifaketory.apiary-mock.com/api/user?limit=" + limit;
-        UserData userData = restTemplate.getForObject(url, UserData.class);
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(apiUrl)
+                .queryParam("limit", limit)
+                .build()
+                .toUri();
+        UserData userData = restTemplate.getForObject(uri, UserData.class);
 
         return userData.getData();
     }
--- src/main/java/us/vicentini/springrestclientexamples/services/ApiServiceReactiveImpl.java
+++ src/main/java/us/vicentini/springrestclientexamples/services/ApiServiceReactiveImpl.java
@@ -1,6 +1,7 @@
 package us.vicentini.springrestclientexamples.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,18 +14,14 @@ import us.vicentini.springrestclientexamples.api.domain.UserData;
 @RequiredArgsConstructor
 @ConditionalOnMissingBean(RestTemplate.class)
 class ApiServiceReactiveImpl implements ApiService {
-
-    private static final String EXTERNAL_SERVICE =
-            "http://private-anon-1eb7500bda-apifaketory.apiary-mock.com/api/user?limit=";
+    @Value("${api.url}")
+    private String apiUrl;
 
 
     @Override
     public Flux<User> getUsersReactive(Integer limit) {
-
-        String url = EXTERNAL_SERVICE + limit;
-
-        return WebClient.builder().build().get()
-                .uri(url)
+        return WebClient.builder().baseUrl(apiUrl).build().get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit).build())
                 .retrieve()
                 .bodyToFlux(UserData.class)
                 .flatMap(userData -> Flux.fromIterable(userData.getData()));
